e3a3bbdf93ef8b0d5aafbc7ae0ffb54ea1a1f798
Hiroyasu Tawara <zen_qp_mai@icloud.com>
2022-10-31 18:28:08 +0900

    Add:タスク投稿時のテストを作成、バリデーションを追加

--- Gemfile
+++ Gemfile
@@ -44,7 +44,6 @@ end
 
 group :test do
   gem 'capybara', '>= 3.26'
-  gem 'selenium-webdriver', '>= 4.0.0.rc1' 
   gem 'webdrivers'
 end
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   enum priolity: { low: 0, middle: 1, high: 2, top: 3 }
   enum status: { waiting: 0, working: 1, completed: 2 }
-
+  validates :name,  presence: true, length: { maximum: 32}
+  validates :note, length: { maximum: 1024}
 end
