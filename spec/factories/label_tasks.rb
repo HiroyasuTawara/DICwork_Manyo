8cf77ace71a503c555a749e6beef8cf61c5df116
HiroyasuTawara <45650777+HiroyasuTawara@users.noreply.github.com>
2022-11-14 11:03:33 +0900

    rspecエラー対処中

--- spec/factories/label_tasks.rb
+++ spec/factories/label_tasks.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :label_task do
-    
+    task { nil }
+    label { nil }
   end
 end
--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :label do
-    name { "MyString" }
+    name { "ラベルテスト" }
+  end
+
+  factory :first_label, class: Label do
+    name { 'ラベル1' }
+    user_id { }
+  end
+
+  factory :second_label, class: Label do
+    name { 'ラベル2' }
+    user_id { }
   end
 end
